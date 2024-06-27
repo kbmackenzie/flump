38e6dee36d358fff3a9b10db997fe662285d914b
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-27 07:10:35 -0300

    feat: Add better download logic, reading filename from headers

--- src/download-image.js
+++ src/download-image.js
@@ -1,13 +1,22 @@
 import fetch from "node-fetch";
 import { createWriteStream } from "node:fs";
 import { pipeline } from "node:stream/promises";
+import { parse as parseContentDisposition } from "content-disposition";
+import { join as joinPath } from "node:path";
 
-export const downloadImage = async (url, outputPath) => {
+export const downloadImage = async (url, destination) => {
   const response = await fetch(url);
   const imageStream = response.body;
 
+  const contentDisposition = response.headers.get('Content-Disposition');
+  if (!contentDisposition) {
+    throw new Error(`Response is missing 'Content-Disposition' header! | url: '${url}'`);
+  }
+  const filename   = parseContentDisposition(contentDisposition).parameters.filename;
+  const targetPath = joinPath(destination, filename);
+
   await pipeline(
     imageStream,
-    createWriteStream(outputPath),
+    createWriteStream(targetPath),
   );
 };
