55105f6c0cd4b5f0344d1c9da554b0645463a534
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-30 04:36:45 -0300

    feat(scraper): Ensure response's MIME type is an image type

--- src/scraper/download-image.js
+++ src/scraper/download-image.js
@@ -4,13 +4,28 @@ import { pipeline } from "node:stream/promises";
 import { parse as parseContentDisposition } from "content-disposition";
 import { join as joinPath } from "node:path";
 
+const imageTypes = new Set([
+  'image/apng',
+  'image/avif',
+  'image/gif',
+  'image/jpeg',
+  'image/png',
+  'image/svg',
+  'image/webp',
+]);
+
 export const downloadImage = async (url, destination) => {
   const response = await fetch(url);
   const imageStream = response.body;
 
+  const contentType = response.headers.get('Content-Type');
+  if (!contentType || !imageTypes.has(contentType.toLowerCase())) {
+    throw new Error(`Response's MIME type isn't a valid image type! | url: ${url}`);
+  }
+
   const contentDisposition = response.headers.get('Content-Disposition');
   if (!contentDisposition) {
-    throw new Error(`Response is missing 'Content-Disposition' header! | url: '${url}'`);
+    throw new Error(`Response is missing 'Content-Disposition' header! | url: ${url}`);
   }
   const filename   = parseContentDisposition(contentDisposition).parameters.filename;
   const targetPath = joinPath(destination, filename);
