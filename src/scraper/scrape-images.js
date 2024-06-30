9e8fadcf1f03912a14840b01ab99515e6e0f5dda
kbmackenzie <kelly.a.betty@gmail.com>
2024-06-30 03:07:14 -0300

    feat(scraper): Simplify thumbnail selector

--- src/scraper/scrape-images.js
+++ src/scraper/scrape-images.js
@@ -1,4 +1,4 @@
-const thumbSelector = 'a.image.lightbox';
+const thumbSelector = 'a.image';
 const sourceSelector = '#LightboxModal .see-full-size-link';
 
 const findImages = (page) => page.evaluate(
