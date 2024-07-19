3f8ffb275c5103e94e8778e6291905345fde56eb
kbmackenzie <kelly.a.betty@gmail.com>
2024-07-19 03:55:05 -0300

    feat: Add logger creation util

---
+++ src/logger.js
@@ -0,0 +1,18 @@
+import winston from "winston";
+
+export const createLogger = (debug) => winston.createLogger({
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+      level: debug ? 'debug' : 'info',
+    }),
+    new winston.transports.File({
+      format: winston.format.json(),
+      filename: 'flump-output.log',
+      level: debug ? 'debug' : 'info',
+    }),
+  ],
+});
