0ed27c75531acf646a2e8f9c109e4bd61fce8536
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-13 21:58:29 -0300

    docs: Add api documentation as jsdoc

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
 import { initLogger } from './logger.js';
 import * as scraper from './scraper/index.js';
 
+/** 
+ * @typedef {Object} FlumpOptions
+ * @prop {boolean} quiet - silence log messages
+ * @prop {string} output - output folder for images
+ */
+
+/**
+ * Scrape a Fandom wiki page for the full-size URLs of gallery images.
+ * @param {string} url - URL to a Fandom character page.
+ * @param {FlumpOptions} options - extra options
+ * @returns {Promise<string[]>}
+ */
 export async function scrapeImages(url, options = {}) {
   const logger = initLogger(!!options.quiet, destination);
   await scraper.scrapeImages(url, logger);
 };
 
+/**
+ * Scrape a Fandom wiki page and download all images from galleries.
+ * @param {string} url - URL to a Fandom character page.
+ * @param {FlumpOptions} options - extra options
+ * @returns {Promise<void>}
+ */
 export async function downloadImages(url, options = {}) {
   const logger = initLogger(!!options.quiet, destination);
   const destination = options.output || './images/';
