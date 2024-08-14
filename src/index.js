68f719df5cacc4b0154dbc401fa7d9d4b874c1da
kbmackenzie <kelly.a.betty@gmail.com>
2024-08-13 21:55:08 -0300

    fix: Fix wrong number of parameters passed to scraper.scrapeImage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import { initLogger } from './logger.js';
 import * as scraper from './scraper/index.js';
 
 export default async (url, options = {}) => {
-  const destination = options.output || './images/';
-  const logger      = initLogger(!!options.quiet, destination);
-  await scraper.scrapeImages(url, destination, logger);
+  const logger = initLogger(!!options.quiet, destination);
+  await scraper.scrapeImages(url, logger);
 };
 
 export const downloadImages = async (url, options = {}) => {
+  const logger = initLogger(!!options.quiet, destination);
   const destination = options.output || './images/';
-  const logger      = initLogger(!!options.quiet, destination);
   await scraper.downloadImages(url, destination, logger);
 };
